bfec7c10350904ea9ffa5b2d897bfd7639c7cbe2
Lautaro Garcia <lautaroegarcia1995@gmail.com>
2023-06-01 16:02:17 -0300

    Update Ejercicio_1.c
    Realizar un algoritmo para dividir dos números naturales mediante restas sucesivas utilizando la estructura WHILE

--- Ejercicio_1.c
+++ Ejercicio_1.c
@@ -4,21 +4,6 @@
 void main () {
 
     /*Realizar un algoritmo para dividir dos números naturales mediante restas sucesivas utilizando la estructura WHILE
-
-
-
-
-Inicializa una variable llamada cociente en cero, que representa la respuesta de la división.
-
-Resta el divisor del dividendo tantas veces como sea posible, siempre y cuando el dividendo sea mayor o igual al divisor.
-
-Después de cada resta exitosa, incrementa el cociente en uno.
-
-Continúa restando el divisor del dividendo hasta que ya no sea posible.
-
-El valor final del cociente es el resultado de la división.
-
-
      */
 
     int dividendo, aux, divisor, resultado, cociente = 0;
